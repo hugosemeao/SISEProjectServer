e65f4ed1a34bd47ccaa6f2621adb8426782fb1a4
hsemeao <hmsemeao@gmail.com>
2020-01-27 15:40:34 +0000

    Server v1.0

--- src/com/insure/server/Main.java
+++ src/com/insure/server/Main.java
@@ -1,7 +1,11 @@
 package com.insure.server;
 
+import javax.xml.ws.Endpoint;
+
 public class Main {
     public static void main(String args[]){
         System.out.println("Project template - server");
+        ClaimDataStore claim = new ClaimDataStore();
+        Endpoint.publish("http://146.193.7.111:8090/docstorage", claim);
     }
 }
